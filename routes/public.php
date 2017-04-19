8fcbe52d836c10e5235979f636db92e92632e7ce
krasho <crasho1481@gmail.com>
2017-04-19 18:48:27 -0500

    I created the list option, includes test, view and changes in PostController

--- routes/public.php
+++ routes/public.php
@@ -12,9 +12,10 @@
 */
 
 
-Route::get('/', function() {
-    return view('welcome');
-});
+Route::get('/', [
+    'uses' => 'PostController@index',
+    'as' => 'posts.index'
+]);
 
 Auth::routes();
 
